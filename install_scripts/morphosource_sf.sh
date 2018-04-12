e1f9fb89145f2e5e4421357e60d0d7828d33b542
Simon Choy <simon.choy@duke.edu>
2018-04-12 13:25:38 -0400

    removed app setup from vagrantfile

--- install_scripts/morphosource_sf.sh
+++ install_scripts/morphosource_sf.sh
@@ -9,12 +9,18 @@ echo '-------------'
 
 cd /vagrant/MorphoSource_SF
 
+echo 'Install bundler and run bundle install...'
+
+#read -p "Press enter to continue "
+gem install bundler
+which bundle
+
+#read -p "Press enter to continue "
+bundle install --without production
+
+#read -p "Press enter to continue "
 echo 'Copying the role map config file...'
 cp config/role_map.yml.sample config/role_map.yml
-
-echo 'Install bundler and run bundle install...'
-sudo gem install bundler
-bundle install --without production
 
 echo 'run database migrations...'
 rake db:migrate
@@ -24,7 +30,5 @@ rake hyrax:workflow:load
 
 echo '-------------'
 echo 'Setup completed.  To start the server:'
-echo 'vagrant ssh'
-echo 'cd /vagrant/MorphoSource_SF'
 echo 'bin/rails hydra:server'
 echo '-------------'
