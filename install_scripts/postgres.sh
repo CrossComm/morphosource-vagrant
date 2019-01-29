d327f2ff4454398b107ee48c553d37853e145c6e
Ryan Baumann <ryan.baumann@gmail.com>
2019-01-29 16:30:02 -0500

    Update PostgreSQL install script to create both "ubuntu" and "vagrant" superusers
    This fixes an error in some Vagrant environments which have an "ubuntu" username instead of a "vagrant" username. There shouldn't be any negatives from adding both users.

--- install_scripts/postgres.sh
+++ install_scripts/postgres.sh
@@ -8,5 +8,5 @@ sudo apt-get -y install $PACKAGES
 
 # As our vagrant box defaults to a user named 'ubuntu',
 # we have to create a corresponding 'ubuntu' SUPERUSER in PostgreSQL
-#sudo -u postgres createuser ubuntu --superuser || true
+sudo -u postgres createuser ubuntu --superuser || true
 sudo -u postgres createuser vagrant --superuser || true
