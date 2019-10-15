de21e643fb1a2e41f0580d75dda5545605030416
Julie Winchester <julie.winchester@duke.edu>
2019-10-15 16:40:43 -0400

    Updated python install script

--- install_scripts/python.sh
+++ install_scripts/python.sh
@@ -2,9 +2,6 @@
 
 echo "Installing necessary Python modules for MorphoSourceSF"
 
-cd
-echo "alias python=python3" >> .bashrc
-
 sudo python3 -m pip install numpy
 echo "Check numpy Installation"
 python3 -c 'import numpy; print(numpy.__version__)'
@@ -16,7 +13,3 @@ python3 -c 'import PIL; print(PIL.__version__)'
 sudo python3 -m pip install pydicom
 echo "Check pydicom Installation"
 python3 -c 'import pydicom; print(pydicom.__version__)'
-
-
-
-
