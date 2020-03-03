6ff170fc059bb53c7aa3082136fc785763abedd0
Simon Choy <simon.choy@duke.edu>
2020-03-03 09:41:44 -0500

    vagrant changes for blender 2.8.2

--- install_scripts/node.sh
+++ install_scripts/node.sh
@@ -7,8 +7,12 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install --lts
-nvm use --lts
+#  downgrade node JS to 10.16.3 because of ES module error.  Match node version with dev/demo
+#  https://github.com/CesiumGS/gltf-pipeline/issues/507
+#nvm install --lts
+#nvm use --lts
+nvm install 10.16.3
+nvm use 10.16.3
 npm install -g npm
 
 
