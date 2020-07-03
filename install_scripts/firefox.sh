ebd2c3e86a23cad8aa108bf0b95107557d5cb796
Julie Winchester <julie.winchester@duke.edu>
2020-07-03 17:58:10 -0500

    Upgrade Ubuntu to 18.04

--- install_scripts/firefox.sh
+++ install_scripts/firefox.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-SHARED_DIR=$1
-
-if [ -f "$SHARED_DIR/install_scripts/config" ]; then
-  . $SHARED_DIR/install_scripts/config
-fi
-
-if [ ! -d /opt/firefox ]; then
-  DOWNLOAD_URL="https://download-installer.cdn.mozilla.net/pub/firefox/releases/$FIREFOX_VERSION/linux-x86_64/en-US/firefox-$FIREFOX_VERSION.tar.bz2"
-  cd $DOWNLOAD_DIR
-  if [ ! -f "firefox.tar.bz2" ]; then
-    echo "Downloading $DOWNLOAD_URL"
-    curl -sS $DOWNLOAD_URL -o firefox.tar.bz2
-  fi
-  tar xjf firefox.tar.bz2
-  sudo mv firefox /opt/firefox
-fi
+sudo apt-get -y install firefox
--- install_scripts/java.sh
+++ install_scripts/java.sh
@@ -7,5 +7,5 @@ if which java >/dev/null; then
   echo "skip java 8 installation"
 else
   echo "java 8 installation"
-  apt-get install --yes default-jdk
+  apt-get install --yes openjdk-8-jdk
 fi
